a4b076363827ed6dafa431a0d5fce9e88d12bc43
Abdalkadir <abdelgzali@gmail.com>
2020-10-01 11:06:09 -0700

    some ui

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import QRCode from 'qrcode.react';
+// import Slider from 'react-slick';
 
 const coinPrefix = {
   bitcoin: 'bitcoin:',
@@ -10,6 +11,7 @@ const coinPrefix = {
 export default function Profiles() {
   const profileData = {
     userName: 'Michael Scott',
+    userHandle: 'michaelscott',
     userID: 100001,
     avatarURL:
       'https://www.arivalevent.com/wp-content/uploads/2018/06/michael-scott.jpg',
@@ -27,7 +29,7 @@ export default function Profiles() {
       {
         label: 'BCH Main',
         address: 'bitcoincash:qrt8d7zl46wlarslt2p9ha357vzg9xfn2ydfptr08v',
-        coinType: 'bitcoin cash',
+        coinType: 'bitcoincash',
       },
     ],
   };
@@ -64,17 +66,19 @@ export default function Profiles() {
 
   return (
     <div id="profile">
-      <figure>
-        <img src={profileData.avatarURL} alt="avatar" />
-        <figcaption>
-          <h2>{profileData.userName}</h2>
-        </figcaption>
+      <header className="profile-header">
+        <figure>
+          <img src={profileData.avatarURL} alt="avatar" />
+          <figcaption>
+            <h2>{profileData.userName}</h2>
+            <p>{`@${profileData.userHandle}`}</p>
+            <button>FOLLOW</button>
+          </figcaption>
+        </figure>
+      </header>
 
-        <div>
-          <button>FOLLOW</button>
-        </div>
-      </figure>
       <section>
+        <h3>Wallets</h3>
         <ul>
           {profileData.wallets.map((wallet, index) => {
             return (
@@ -89,7 +93,16 @@ export default function Profiles() {
                     )}
                   </p>
                 </div>
-                <QRCode value={createURI(wallet)} className="qrcode"/>
+                <img src="../assets/images/bitcoin-logo.svg" alt="" />
+                <QRCode
+                  value={createURI(wallet)}
+                  imageSettings={{
+                    src: `/images/${wallet.coinType}-logo.svg`,
+                    height: 35,
+                    width: 35,
+                  }}
+                  className="qrcode"
+                />
               </li>
             );
           })}
